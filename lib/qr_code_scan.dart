2687f292c6d8eac6b375d06b27c2423eea75e80b
eltonpinto156 <56260222+eltonpinto156@users.noreply.github.com>
2020-08-03 15:56:17 +0530

    Update qr code

--- lib/qr_code_scan.dart
+++ lib/qr_code_scan.dart
@@ -1,74 +1,119 @@
-import 'package:barcode_scan/barcode_scan.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
-class ScanPage extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:tech_bin/widgets/entry.dart';
+import 'package:tech_bin/widgets/loader.dart';
+import 'package:lottie/lottie.dart';
+
+int randomNumber;
+
+class Scanner extends StatefulWidget {
   @override
-  _ScanPageState createState() => _ScanPageState();
+  _ScannerState createState() => new _ScannerState();
 }
 
-class _ScanPageState extends State<ScanPage> {
-  String qrCodeResult = "Not Yet Scanned";
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Scanner"),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              "Result",
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            Text(
-              qrCodeResult,
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            FlatButton(
-              padding: EdgeInsets.all(15.0),
-              onPressed: () async {
+class _ScannerState extends State<Scanner> {
+  String result = "h";
+  bool loader = true, succes = false;
+  Random random = new Random();
 
+  Future _scanQR() async {
+    try {
+      var qrResult = await BarcodeScanner.scan();
 
-                var codeSanner = await BarcodeScanner.scan();    //barcode scnner
-                setState(() {
-                  qrCodeResult = codeSanner as String;
-                });
-
-                // try{
-                //   BarcodeScanner.scan()    this method is used to scan the QR code
-                // }catch (e){
-                //   BarcodeScanner.CameraAccessDenied;   we can print that user has denied for the permisions
-                //   BarcodeScanner.UserCanceled;   we can print on the page that user has cancelled
-                // }
-
-
-              },
-              child: Text(
-                "Open Scanner",
-                style:
-                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-              ),
-              shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.blue, width: 3.0),
-                  borderRadius: BorderRadius.circular(20.0)),
-            )
-          ],
-        ),
-      ),
-    );
+      print("result${qrResult.rawContent}..\n");
+      if ((qrResult.type).toString() != 'Cancelled') {
+        setState(() {
+          result = qrResult.rawContent;
+          succes = true;
+          randomNumber = int.parse(result);
+          print(randomNumber);
+          print((qrResult.type).toString());
+        });
+      } else {
+        succes = false;
+        print(succes);
+        route();
+      }
+    } catch (ex) {
+      setState(() {
+        result = "ex";
+        succes = false;
+      });
+    }
+    loader = false;
   }
 
+  @override
+  void initState() {
+    super.initState();
+    _scanQR();
+  }
 
+  startTime() async {
+    var duration = new Duration(seconds: 5);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    if (succes == true) {
+      print(result);
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => EntryPage(
+                    selectedIndex: 1,
+                  )));
+    } else {
+      print("home");
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => EntryPage(
+            selectedIndex: 0,
+          ),
+        ),
+      );
+    }
+  }
+
+  // ignore: missing_return
+  Widget build(BuildContext context) {
+    switch (loader) {
+      case true:
+        return Loader();
+      case false:
+        startTime();
+        return Scaffold(
+          body: Center(
+            child: succes == true
+                ? Center(
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      child: Column(
+                        children: [
+                          Lottie.asset('assets/animations/weight.json'),
+                          Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: Text(
+                              'Estimating the Weight',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Text(
+                    "scan unSuccessfull!!!!!!!!$result",
+                    style: TextStyle(fontSize: 20),
+                  ),
+          ),
+        );
+    }
+  }
 }
